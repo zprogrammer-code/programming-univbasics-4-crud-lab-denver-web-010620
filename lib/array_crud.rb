01ef9770d6e955fbceb43719a79b5a3916dca208
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-20 05:28:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -39,7 +39,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+   array = ["wow","am"]
+  return array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
