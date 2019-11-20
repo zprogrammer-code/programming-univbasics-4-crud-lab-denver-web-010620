574c1e524ab6c1409c6c53c8c28851c8dfd1fc01
Zachary Jacobs <skatboyza124@hotmail.com>
2019-11-20 05:26:31 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -29,7 +29,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array = ["howard","jeff"]
+  array = ["howard","am"]
   return array[1]
 end
 
